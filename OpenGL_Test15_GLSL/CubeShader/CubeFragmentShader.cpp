c51d452787d7deb2265cb21d62169ce7de4c4f01
LiangHaoXiang <1013568397@qq.com>
2021-08-02 16:16:07 +0800

    接口块，统一管理顶点着色器和片段着色器之间传输的变量

--- OpenGL_Test15_GLSL/CubeShader/CubeFragmentShader.cpp
+++ OpenGL_Test15_GLSL/CubeShader/CubeFragmentShader.cpp
@@ -1,16 +1,18 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 Normal;
-in vec3 Position;
+in VS_OUT
+{
+    vec3 Normal;
+    vec3 Position;
+} fs_in;
 
 uniform vec3 cameraPos;
 uniform samplerCube skybox;
 
 void main()
 {
-    gl_FragDepth = 0.0; // 这个片段现在的深度值为 0.0
-    vec3 I = normalize(Position - cameraPos);
-    vec3 R = reflect(I, normalize(Normal));
+    vec3 I = normalize(fs_in.Position - cameraPos);
+    vec3 R = reflect(I, normalize(fs_in.Normal));
     FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
